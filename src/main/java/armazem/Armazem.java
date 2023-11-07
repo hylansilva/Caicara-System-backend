515ac68760951d75c2d6f29b14b770b06ae07300
Sabrina Costa <sabrina.00000847856@unicap.br>
2023-11-07 19:25:45 -0300

    Sabrina Costa - Criando as classes Cooperado e Armazem

--- src/main/java/armazem/Armazem.java
+++ src/main/java/armazem/Armazem.java
@@ -1,4 +1,69 @@
 package armazem;
 
+import users.Funcionario;
+
 public class Armazem {
+    private int numeroArmazem;
+    private String localizacao;
+    private double capacidadeTon;
+    private double capacidadeMetrosQuadrados;
+    private Funcionario gerenteResponsavel;
+
+    public Armazem() {
+    }
+
+    public Armazem(int numeroArmazem, String localizacao, double capacidadeTon, double capacidadeMetrosQuadrados, Funcionario gerenteResponsavel) {
+        this.numeroArmazem = numeroArmazem;
+        this.localizacao = localizacao;
+        this.capacidadeTon = capacidadeTon;
+        this.capacidadeMetrosQuadrados = capacidadeMetrosQuadrados;
+        this.gerenteResponsavel = gerenteResponsavel;
+    }
+
+    public Armazem(int numeroArmazem, String localizacao, double capacidadeTon, double capacidadeMetrosQuadrados) {
+        this.numeroArmazem = numeroArmazem;
+        this.localizacao = localizacao;
+        this.capacidadeTon = capacidadeTon;
+        this.capacidadeMetrosQuadrados = capacidadeMetrosQuadrados;
+    }
+
+    public int getNumeroArmazem() {
+        return numeroArmazem;
+    }
+
+    public void setNumeroArmazem(int numeroArmazem) {
+        this.numeroArmazem = numeroArmazem;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public double getCapacidadeTon() {
+        return capacidadeTon;
+    }
+
+    public void setCapacidadeTon(double capacidadeTon) {
+        this.capacidadeTon = capacidadeTon;
+    }
+
+    public double getCapacidadeMetrosQuadrados() {
+        return capacidadeMetrosQuadrados;
+    }
+
+    public void setCapacidadeMetrosQuadrados(double capacidadeMetrosQuadrados) {
+        this.capacidadeMetrosQuadrados = capacidadeMetrosQuadrados;
+    }
+
+    public Funcionario getGerenteResponsavel() {
+        return gerenteResponsavel;
+    }
+
+    public void setGerenteResponsavel(Funcionario gerenteResponsavel) {
+        this.gerenteResponsavel = gerenteResponsavel;
+    }
 }
--- src/main/java/users/Cooperado.java
+++ src/main/java/users/Cooperado.java
@@ -1,5 +1,6 @@
 package users;
 
+import armazem.Armazem;
 import conta.Conta;
 
 public class Cooperado extends User{
@@ -9,6 +10,84 @@ public class Cooperado extends User{
      private String quantidadeFamiliares;
      private String endereco;
      private String telefone;
+     private Armazem armazem;
 
+     public Cooperado() {
+     }
 
+     public Cooperado(String nome, String email, String codigoCooperativa, String senha, Conta conta, String rg, String cpf, String quantidadeFamiliares, String endereco, String telefone, Armazem armazem) {
+          super(nome, email, codigoCooperativa, senha);
+          this.conta = conta;
+          this.rg = rg;
+          this.cpf = cpf;
+          this.quantidadeFamiliares = quantidadeFamiliares;
+          this.endereco = endereco;
+          this.telefone = telefone;
+          this.armazem = armazem;
+     }
+
+     public Cooperado(String nome, String email, String codigoCooperativa, String senha, String rg, String cpf, String quantidadeFamiliares, String endereco, String telefone) {
+          super(nome, email, codigoCooperativa, senha);
+          this.rg = rg;
+          this.cpf = cpf;
+          this.quantidadeFamiliares = quantidadeFamiliares;
+          this.endereco = endereco;
+          this.telefone = telefone;
+     }
+
+     public Conta getConta() {
+          return conta;
+     }
+
+     public void setConta(Conta conta) {
+          this.conta = conta;
+     }
+
+     public String getRg() {
+          return rg;
+     }
+
+     public void setRg(String rg) {
+          this.rg = rg;
+     }
+
+     public String getCpf() {
+          return cpf;
+     }
+
+     public void setCpf(String cpf) {
+          this.cpf = cpf;
+     }
+
+     public String getQuantidadeFamiliares() {
+          return quantidadeFamiliares;
+     }
+
+     public void setQuantidadeFamiliares(String quantidadeFamiliares) {
+          this.quantidadeFamiliares = quantidadeFamiliares;
+     }
+
+     public String getEndereco() {
+          return endereco;
+     }
+
+     public void setEndereco(String endereco) {
+          this.endereco = endereco;
+     }
+
+     public String getTelefone() {
+          return telefone;
+     }
+
+     public void setTelefone(String telefone) {
+          this.telefone = telefone;
+     }
+
+     public Armazem getArmazem() {
+          return armazem;
+     }
+
+     public void setArmazem(Armazem armazem) {
+          this.armazem = armazem;
+     }
 }
